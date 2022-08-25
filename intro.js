af2cc87e0e7e5c9a3182c10701999aaa2a0fc0e7
ashiikkhan <ashiksust06@gmail.com>
2022-08-25 21:27:03 +0600

    perfect

---
+++ intro.js
@@ -0,0 +1,37 @@
+/**What is Array ?
+ * Array is a data structure. Array is Just A Collection.
+ * Array is a data structure consisting of collection of elements, each edentified by at least one array index or key.
+ *
+ **Why we need Array? Why we don't use variable instead of array?
+ * we can write variable with a single value but for storing multiple values and elements in a single variable we use array.
+ *
+ **How is Array?
+ * we can access array elements by index.
+ * for example we have 100 products. if we put data individually in a variable and then try to do something on these data it will be tough; but if we put all products in an array we can easy automate the data from array.
+ *
+ ** How does array work in JS?
+ * step-1 create an instance : let arr = [];
+ *
+ * step-2 add elements :
+ * arr[0] = 45
+ * arr[1] = 30
+ * arr[2] = 20
+ * our array will look like : [45, 30, 20]
+ *
+ * step-3 access data when necessary:
+ * let sum = 0;
+ * sum += arr[0]
+ * sum += arr[1]
+ * sum += arr[2]
+ *
+ * JavaScript Array is Dynamic;
+ * JavaScript Array can store any kind of data;
+ *
+ ** Multi-dimensional array
+ * let multiDimenArr = [
+ *  [2, 3, 4],
+ *  [5, 6, 7]
+ * ]
+ * to get 6 from this array we need to write: multiDimenArr[j][i] => multiDimenArr[1][4] => 6;
+ *
+ */
